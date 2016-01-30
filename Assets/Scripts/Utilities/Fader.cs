c192615774f0bef1235dee994c5293d18eef41d9
RVerhoef <r.verhoef@hotmail.nl>
2016-01-30 23:08:52 +0100

    Added dev sprites

---
+++ Assets/Scripts/Utilities/Fader.cs
@@ -0,0 +1,26 @@
+﻿//Written by Rob Verhoef
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Fader : MonoBehaviour
+{
+    private Image _fade;
+
+    void Awake ()
+    {
+        _fade = GameObject.Find("Fade").GetComponent<Image>();
+    }
+
+    void Start ()
+    {
+        //The screen fades in
+        _fade.CrossFadeColor(Color.clear, 5.0f, true, true);
+    }
+
+    public void FadeIn ()
+    {
+        //The screen fades out
+        _fade.CrossFadeColor(Color.black, 1.5f, true, true);
+    }
+}
